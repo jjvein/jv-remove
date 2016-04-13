ab32d74eab893f87103a946a6a05d78188e09693
jjvein <jjvein@gmail.com>
2016-04-13 11:13:08 +0800

    add test

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # jv-remove
+---
+
+Remove elements from an array without Array.prototype.splice
+
+### Usage
+
+```
+var remove = require('jv-remove')
+remove(arr, startOffset, length)
+```
+`arr`: Array need to be handled  
+`startOffset`: start offset of the array, must less the length of array  
+`length`: number of the elements to be removed, default to 1
+
+```
+var remove = require('jv-remove')
+var arr = [1, 2, 3, 4, 5]
+var ret = remove(arr, 1, 2)
+console.log('arr==>', arr)
+console.log('ret==>', ret)
+```
+
+### MIT
+
+
+### Referer
+I got this inspiration from [mafintosh's repo](https://github.com/mafintosh/unordered-array-remove). Just want to enhance this idea to make it powerful.
+
--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 "use strict"
 
 module.exports = function(arr, i, count){
-    if(!arr.length) return arr
-    var lg = console.log
+
+    //start offset must be the index of an array
+    if(!arr.length || arr.length <= i) return false
+
     count = ~~count > 0 ? ~~count : 1
     var len = arr.length
 
-    if(len < count) return arr
-
     var tmp=[], ret = []
+    var left = len - i - count
+    var retReserve = left > 0 ? (left < count ? left : count) : 0
+    var loop = left > 0 ? count : (len-i)
 
-    for(var j=0; j<count; j++) {
+    for(var j=0; j<loop; j++) {
       tmp.push(arr[i+j])  
     }
-    var retReserve = (len - i - count) > 0 ? (len - i - count) : 0
-    var max = count > retReserve ? count : retReserve
-    for(var k=0; k<max; k++) {
+
+    while(loop--) {
         ret.push(arr.pop())
     }
+
     for(var l=0; l<retReserve; l++) {
       arr[l+i]  = ret[l]
     }
-
     return tmp
 }
